2de17592e77033140fe36ddff7422c8774be8dc5
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2025-03-20 23:37:53 +0000

    Add reset and collision detection methods to SnakeGame class

--- game.py
+++ game.py
@@ -49,12 +49,76 @@ class SnakeGame:
         self.food = None
         self._place_food()
 
+    def reset(self):
+        self.direction = Direction.RIGHT
+
+        self.head = Point(self.w / 2, self.h / 2)
+        self.snake = [
+            self.head,
+            Point(self.head.x - BLOCK_SIZE, self.head.y),
+            Point(self.head.x - (2 * BLOCK_SIZE), self.head.y),
+        ]
+        self.score = 0
+        self.food = None
+        self._place_food()
+        self.frame_iteration = 0
+
     def _place_food(self):
         x = random.randint(0, (self.w - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
         y = random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
         self.food = Point(x, y)
         if self.food in self.snake:
             self._place_food()
+
+    def play_step_2(self, action):
+        self.frame_iteration += 1
+        # 1. collect user input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            # 2. move
+            self._move(action)
+            self.snake.insert(0, self.head)
+
+            # 3. check if game over
+            reward = 0
+            game_over = False
+            if self.is_collision() or self.frame_iteration > 100 * len(self.snake):
+                game_over = True
+                reward = -10
+                return reward, game_over, self.score
+
+            # 4. place new food or just move
+            if self.head == self.food:
+                self.score += 1
+                reward = 10
+                self._place_food()
+            else:
+                self.snake.pop()
+
+            # 5. update ui and clock
+            self._update_ui()
+            self.clock.tick(SPEED)
+            # 6. return game over and score
+            return reward, game_over, self.score
+
+    def is_collision(self, pt=None):
+        if pt is None:
+            pt = self.head
+        # hits boundary
+        if (
+            pt.x > self.w - BLOCK_SIZE
+            or pt.x < 0
+            or pt.y > self.h - BLOCK_SIZE
+            or pt.y < 0
+        ):
+            return True
+        # hits itself
+        if pt in self.snake[1:]:
+            return True
+        return False
 
     def play_step(self):
         for event in pygame.event.get():
